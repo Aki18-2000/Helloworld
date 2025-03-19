2e207f692d16095c408639d14606543278cf7d7a
Aki18-2000 <makshaya206@gmail.com>
2025-03-19 15:23:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,40 @@ provider "aws" {
   region = "us-east-1"  # Change to your preferred AWS region
 }
 
-resource "aws_instance" "docker_instance" {
-  ami           = "ami-08b5b3a93ed654d19"  # Use an appropriate Amazon Linux 2 AMI ID
-  instance_type = "t2.micro"  # Instance type
+# Security Group to allow HTTP traffic on port 80
+resource "aws_security_group" "docker_sg" {
+  name        = "docker_sg"
+  description = "Allow HTTP traffic on port 80"
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from anywhere (you can limit this to specific IPs)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
+  }
+
+  tags = {
+    Name = "docker_sg"
+  }
+}
+
+# EC2 instance running Docker
+resource "aws_instance" "docker_instance" {
+  ami           = "ami-08b5b3a93ed654d19"  # Amazon Linux 2 AMI (ensure this is valid for your region)
+  instance_type = "t2.micro"  # Instance type (you can change it if needed)
+  
+  
+  # Attach the security group to the EC2 instance
+  security_groups = [aws_security_group.docker_sg.name]
+
+  # User data script to install Docker, run Nginx container, and create the HTML page
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
@@ -19,26 +49,24 @@ resource "aws_instance" "docker_instance" {
               # Run Nginx in Docker to serve the Hello World page
               docker run -d -p 80:80 --name webserver -v /home/ec2-user/hello.html:/usr/share/nginx/html/index.html nginx
 
-              # Check Docker container status
+              # Log Docker container status for troubleshooting
               docker ps -a > /home/ec2-user/docker_ps.log
               docker port webserver > /home/ec2-user/docker_port.log
 
-              # Optional: Output the logs to help troubleshoot
+              # Output logs to help troubleshoot
               cat /home/ec2-user/docker_ps.log
               cat /home/ec2-user/docker_port.log
               EOF
 
+  # Optional: Assign a public IP address
+  associate_public_ip_address = true
+
   tags = {
     Name = "docker-instance"
   }
-
-  # Allow HTTP access (Port 80) to the instance
-  security_groups = ["default"]
-
-  # Optional: Assign a public IP address
-  associate_public_ip_address = true
 }
 
+# Output the public IP of the EC2 instance
 output "instance_ip" {
   value = aws_instance.docker_instance.public_ip
 }
